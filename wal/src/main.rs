6ae8f3773a1c9a3a551164233eaabc3102cf842e
zhenren <wangweizhen@bilibili.com>
2018-03-12 20:28:41 +0800

    feat: add usize

--- wal/src/main.rs
+++ wal/src/main.rs
@@ -5,19 +5,22 @@ use zstd::block::compress;
 use hash::{crc16_arr, crc64};
 use std::fs::File;
 use std::io::prelude::*;
+use std::mem::size_of_val;
 
 fn main() {
-	let mut f = File::create("foo.txt");
-	let mut f = match f {
-		Ok(file) => file,
-		Err(e) => {
-			panic!("Failed to open a file: {:?}", e);
-		}
-	};
-	f.write_all(b"aaaaaaaaaaaa");
-	let crc64: [u8; 8] =
-		unsafe { std::mem::transmute(crc64(String::from("aaaaaaaaaaaa").as_bytes())) };
-	f.write_all(&crc64);
-	//f.write_all(&zstd::block::compress("Hello, world!".as_bytes(), 5).unwrap());
-	f.sync_all();
+    let mut f = File::create("foo.txt");
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => {
+            panic!("Failed to open a file: {:?}", e);
+        }
+    };
+    let input = b"aaaaaaaaaaaa";
+    let inputcompress = &zstd::block::compress(input, 5).unwrap();
+    let size = unsafe { std::mem::transmute::<usize, [u8; 8]>(size_of_val(inputcompress)) };
+    f.write_all(&size);
+    f.write_all(inputcompress);
+    let crc64: [u8; 8] = unsafe { std::mem::transmute(crc64(inputcompress)) };
+    f.write_all(&crc64);
+    f.sync_all();
 }
