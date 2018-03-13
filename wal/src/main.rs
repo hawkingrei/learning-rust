a124e9cf0d80b1908db0475361332b035c6b60c3
真人 <weizhen.wang@duitang.com>
2018-03-13 23:57:15 +0800

    WIP

--- wal/src/main.rs
+++ wal/src/main.rs
@@ -22,4 +22,20 @@ fn main() {
 	let crc64: [u8; 8] = unsafe { std::mem::transmute(crc64(inputcompress)) };
 	f.write_all(&crc64);
 	f.sync_all();
+
+	let mut ff = match File::open("foo.txt") {
+		Ok(file) => file,
+		Err(e) => {
+			panic!("Failed to open a file: {:?}", e);
+		}
+	};
+	let mut buffer = Vec::new();
+	ff.read_to_end(&mut buffer);
+	let ssize = unsafe { std::mem::transmute::<[u8; 8], u64>(buffer[0..8]) };
+	let data = &buffer[8..ssize + 8];
+	let crc_expected: u64 = unsafe { std::mem::transmute(buffer[ssize + 8..ssize + 8 + 8]) };
+	let crc_actual = crc64(data);
+	if crc_expected != crc_actual {
+		println!("ok");
+	};
 }
