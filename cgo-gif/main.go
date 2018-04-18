bf01baa7da28ae4131fd5ae422b8a587bbca9907
zhenren <wangweizhen@bilibili.com>
2018-04-18 13:31:05 +0800

    update

--- cgo-gif/main.go
+++ cgo-gif/main.go
@@ -9,11 +9,14 @@ import "C"
 import (
 	"fmt"
 	"io/ioutil"
-	"unsafe"
 )
 
-func get_ff (img []byte,cwidth,cheight _Ctype_short,rptr unsafe.Pointer) _Ctype_int {
-	return C.get_first_frame((*_Ctype_uchar)(C.CBytes(img)),C.ulong(len(img)),&cwidth,&cheight,(*_Ctype_uchar)(rptr))
+func get_ff (img []byte,cwidth,cheight _Ctype_short,imgbuf []byte) {
+	rptr := C.CBytes(imgbuf)
+	imgsize :=  C.get_first_frame((*_Ctype_uchar)(C.CBytes(img)),C.ulong(len(img)),&cwidth,&cheight,(*_Ctype_uchar)(rptr))
+	fmt.Println(rptr)
+	data := C.GoBytes(rptr, imgsize)
+	ioutil.WriteFile("test1.gif",data, 0644)
 }
 
 func main() {
@@ -30,9 +33,5 @@ func main() {
 	cwidth := _Ctype_short(0)
 	cheight := _Ctype_short(0)
 	fmt.Println(rptr)
-	imgsize := get_ff(img,cwidth,cheight,rptr)
-	fmt.Println(rptr)
-	fmt.Println((_Ctype_int)(imgsize))
-	data := C.GoBytes(rptr, imgsize)
-	ioutil.WriteFile("test1.gif",data, 0644)
+	get_ff(img,cwidth,cheight,imgbuf)
 }
