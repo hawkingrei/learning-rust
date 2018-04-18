3964f3aae4520daee2c1babd6393554954adb7b9
zhenren <wangweizhen@bilibili.com>
2018-04-18 11:15:06 +0800

    update

--- cgo-gif/gif/rgif/src/lib.rs
+++ cgo-gif/gif/rgif/src/lib.rs
@@ -48,7 +48,6 @@ pub extern "C" fn get_first_frame(
             };
         }
         let mut f = File::create("test_rust.gif").expect("Unable to create file");
-        println!("{:?}", rptr);
         for i in image.clone() {
             f.write_all((&[i])).expect("Unable to write data");
         }
