a33f98c0182d95fb9b3927419e6a9cfc52eed6dc
zhenren <wangweizhen@bilibili.com>
2018-04-18 13:27:07 +0800

    update

--- cgo-gif/main.go
+++ cgo-gif/main.go
@@ -9,7 +9,12 @@ import "C"
 import (
 	"fmt"
 	"io/ioutil"
+	"unsafe"
 )
+
+func get_ff (img []byte,cwidth,cheight _Ctype_short,rptr unsafe.Pointer) _Ctype_int {
+	return C.get_first_frame((*_Ctype_uchar)(C.CBytes(img)),C.ulong(len(img)),&cwidth,&cheight,(*_Ctype_uchar)(rptr))
+}
 
 func main() {
 	img, err := ioutil.ReadFile("test.gif") // just pass the file name
@@ -25,7 +30,7 @@ func main() {
 	cwidth := _Ctype_short(0)
 	cheight := _Ctype_short(0)
 	fmt.Println(rptr)
-	imgsize := C.get_first_frame((*_Ctype_uchar)(C.CBytes(img)),C.ulong(len(img)),&cwidth,&cheight,(*_Ctype_uchar)(rptr))
+	imgsize := get_ff(img,cwidth,cheight,rptr)
 	fmt.Println(rptr)
 	fmt.Println((_Ctype_int)(imgsize))
 	data := C.GoBytes(rptr, imgsize)
