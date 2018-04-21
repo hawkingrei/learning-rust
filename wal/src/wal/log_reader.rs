49683b403e3a62357840b04ac7ca0add13170393
zhenren <wangweizhen@bilibili.com>
2018-04-21 16:26:34 +0800

    updae

--- wal/src/wal/log_reader.rs
+++ wal/src/wal/log_reader.rs
@@ -24,9 +24,17 @@ pub enum RecordType {
 
 pub struct Reader {
     eof_: bool,
+    buffer_: Vec<u8>,
     read_error_: bool,
+
+    // Offset of the file position indicator within the last block when an
+    // EOF was detected.
     eof_offset_: usize,
+
+    // Offset of the last record returned by ReadRecord.
     last_record_offset_: u64,
+
+    // Offset of the first location past the end of buffer_.
     end_of_buffer_offset_: u64,
     initial_offset_: u64,
     log_number_: u64,
@@ -53,6 +61,40 @@ impl Reader {
             }
         }
         return true;
+    }
+
+    fn readPhysicalRecord(&mut self, fragment: &mut Vec<u8>, drop_size: &mut usize) -> isize {
+        while (true) {}
+        return 0;
+    }
+
+    // For kAbsoluteConsistency, on clean shutdown we don't expect any error
+    // in the log files.  For other modes, we can ignore only incomplete records
+    // in the last log file, which are presumably due to a write in progress
+    // during restart (or from log recycling).
+    //
+    // TODO krad: Evaluate if we need to move to a more strict mode where we
+    // restrict the inconsistency to only the last log
+    fn readRecord(&mut self, record: &mut Vec<u8>, scratch: &mut Vec<u8>) -> bool {
+        if self.last_record_offset_ < self.initial_offset_ {
+            if !(self.SkipToInitialBlock()) {
+                return false;
+            }
+        }
+        record.clear();
+        scratch.clear();
+        let mut in_fragmented_record = false;
+        // Record offset of the logical record that we're reading
+        // 0 is a dummy value to make compilers happy
+        let mut prospective_record_offset = 0;
+
+        let mut fragment: Vec<u8> = Vec::new();
+        while true {
+            let mut physical_record_offset = self.end_of_buffer_offset_ - self.buffer_.len() as u64;
+            let mut drop_size: usize = 0;
+            let record_type = self.readPhysicalRecord(&mut fragment, &mut drop_size);
+        }
+        return false;
     }
 
     //fn ReportDrop(bytes: usize, reason: state) {}
