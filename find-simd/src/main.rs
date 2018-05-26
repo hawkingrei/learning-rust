40e02a9384d782837a9243cf46f1a68dab437dae
zhenren <wangweizhen@bilibili.com>
2018-05-26 16:56:38 +0800

    update

--- find-simd/src/main.rs
+++ find-simd/src/main.rs
@@ -14,15 +14,12 @@ fn main() {
     unsafe {
         let raw_node_key = i8x16::new(12, 43, 5, 0, 6, 4, 3, 44, 35, 74, 37, 35, 19, 74, 69, 54);
         let node_key: __m128i = FromBits::from_bits(raw_node_key);
-        let key = _mm_set1_epi8(8);
+        let key = _mm_set1_epi8(19);
         let cmp = _mm_cmpeq_epi8(key, node_key);
         let mask = (1 << 16) - 1;
         let result = _mm_movemask_epi8(cmp) & mask;
-
-        println!("result {:?}", result);
-        println!("{:?} ,{}", _mm_movemask_epi8(cmp), result & mask);
         if result != 0 {
-            println!("result {:?}", cttz(result));
+            println!("result index {:?}", cttz(result));
         } else {
             println!("result no");
         }
